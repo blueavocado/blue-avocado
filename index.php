ef0f26dccb8533f985675e683a918217e17842b0
ben <turner485@hotmail.co.uk>
2017-09-19 20:42:19 +0100

    push

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="skin/bower_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="skin/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="skin/bootstrap/jasny-bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="skin/css/stars.css">
+	<title>Blue Avocado | Web Design</title>
+</head>
+<html>
+
+<body class="blue-avocado-body">
+	<section class="container-fluid blue-avocado-top">
+		<div class="row wrapper">
+			<div class="col-sm-8 block-left">
+				<img src="" alt="logo" />
+			</div>
+
+			<div class="col-sm-4 block-right">
+				<a href="#" class="btn primary-btn">contact</a>
+			</div>
+		</div>
+
+		<div id='stars'></div>
+		<div id='stars2'></div>
+		<div id='stars3'></div>
+	</section>
+
+	<section class="container-fluid main-banner">
+		<div class="row">
+			<div class="col-sm-12">
+				<img src="images/banner.jpg" class="img-responsive"/>
+			</div>
+		</div>
+	</section>
+
+	<div class="section-bg">
+		<section class="container-fluid blue-avocado-technologies">
+			<div class="row wrapper-head">
+				<div class="col-sm-12 block">
+					<h2>Here are some of the technologies I use!</h2>
+					<span class="line"></span>
+				</div>
+			</div>
+
+			<div class="row wrapper-body">
+				<div class="col-sm-4 block">
+					<img src="images/html.png" class="img-responsive html" />
+				</div>
+				<div class="col-sm-4 block">
+					<img src="images/git.png" class="img-responsive git" />
+				</div>
+				<div class="col-sm-4 block">
+					<img src="images/gulp-bower.png" class="img-responsive gulp" />
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<section class="container-fluid blue-avocado-about-me">
+		<div class="row wrapper">
+			<div class="col-sm-6 block-left">
+			<h2>A tad about me! </h2>
+			<p>Howdy, My Names Benjamin, I'm 24 years old, I have been a professional Web Developer for the last 2 years now, before that I was
+			at the University of Northampton obtaining BSc (Hons) in Computer science.</p>
+			<p>Its only since then i have actually started to develop the skills i have today, as most of my knowledge base comes down to heavy research
+			and real time coding and practice!</p>
+			<p>I'm Currently working full time in central Leicester, but i am also looking to expand and help others achieve success on the web by going freelance in my spare time, As I love personal development and growth!</p>
+			</div>
+			<div class="col-sm-6 block-right">
+				<img src="images/me.jpg" alt="benjamin" class="img-responsive" />
+			</div>
+		</div>
+	</section>
+
+	<section class="container-fluid blue-avocado-specs">
+		<div class="row wrapper-head">
+			<h2>Here what i believe should go into creating a website</h2>
+			<span class="line"></span>
+		</div>
+
+		<div class="row wrapper-body">
+			<div class="col-sm-2 block-mobile">
+				<div class="wrap">
+					<i class="fa fa-mobile-phone"></i>
+					<i class="fa fa-tablet"></i>
+					<i class="fa fa-desktop"></i>
+				</div>
+				<h4>Fully Responsive</h4>
+			</div>
+
+			<div class="col-sm-2 block-user">
+				<div class="wrap">
+					<i class="fa fa-users"></i>
+				</div>
+				<h4>User Friendly</h4>
+			</div>
+
+			<div class="col-sm-2 block-browser">
+				<div class="wrap">
+					<i class="fa fa-firefox"></i>
+					<i class="fa fa-chrome"></i>
+					<i class="fa fa-internet-explorer "></i>
+				</div>
+				<h4>Cross Browser Compatibility</h4>
+			</div>
+
+			<div class="col-sm-2 block-test">
+				<div class="wrap">
+					<i class="fa fa-code"></i>
+					<i class="fa fa-bug"></i>
+				</div>
+				<h4>Fully Optimized</h4>
+			</div>
+
+			<div class="col-sm-2 block-maintain">
+				<div class="wrap">
+					<i class="fa fa-code-fork"></i>
+					<i class="fa fa-cogs"></i>
+				</div>
+				<h4>Ongoing Maintenance</h4>
+			</div>
+
+			<div class="col-sm-2 block-simple">
+				<div class="wrap">
+					<i class="fa fa-check"></i>
+				</div>
+				<h4>Simple & Snappy</h4>
+			</div>
+		</div>
+	</section>
+
+
+	<div id="particles-js">
+		<section class="container-fluid blue-avocado-my-goal">
+			<div class="row">
+				<div class="wrapper clearfix">
+					<div class="col-sm-12">
+						<div class="block">
+							<h2>My Goal</h2>
+							<span class="line"></span>
+							<p>My end goal is to help people bring their businesses to life on the web, through creating an enjoyable online experience for both client & end user<br>
+								As i look to continuously excel my career and experience as a developer, finding new ways to use my creativity and research, using new technologies everyday!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+
+</body>
+
+<footer class="container-fluid footer">
+	<div class="row">
+		<div class="wrapper clearfix">
+			<div class="col-sm-6">
+				<div class="block-left">
+					<h2>Logo Here!!</h2>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="block-right">
+					<i class="fa  fa-envelope-o"></i>
+					<i class="fa  fa-github-square"></i>
+					<i class="fa  fa-gitlab"></i>
+					<i class="fa  fa-facebook"></i>
+					<i class="fa  fa-twitter"></i>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+</html>
+
+<script type="text/javascript" src="skin/bower_components/jquery/dist/jquery.js"></script>
+<script type="text/javascript" src="skin/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
+<script type="text/javascript" src="skin/bootstrap/jasny-bootstrap.js"></script>
+<script type="text/javascript" src="skin/js/particles/particles.js"></script>
+<script type="text/javascript" src="skin/js/particles/app.js"></script>
